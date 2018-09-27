e6034e4fb8aa719699bc143c1e640172deda60c0
Bonkook Koo <djmimi@nate.com>
2018-09-28 01:17:19 +0900

    Refactor 파일 경로 전역 상수로 뺌, new -> {}

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,38 @@
 
     var _styleType;
 
+    const URL_STYLE_DATA = "SampleCircle.json";
+    // const URL_STYLE_DATA = "SampleRoundedRect.json";
+    const URL_COUNTER_STAT_INFO = "CounterInfo.xml";
+    
     $(document).ready(function(){
-      const url = "SampleCircle.json";
-      // const url = "SampleRoundedRect.json";
-      loadJson( url );
+      loadJson( URL_STYLE_DATA );
     });
 
     function loadJson( strUrl ) {
       $.getJSON( strUrl, function( data ) {
-        var styleModel = new StyleModel( data );
+        const defaultValue = {
+          width : 10,
+          height : 10,
+          style : "C",
+          fontfamily : "Daum",
+          titlecolor : "#00ffff",
+          numbercolor : "#00ffff",
+          backgroundcolor : "#00ffff",
+          backgroundalpha : 50,
+          bordertype : 0,
+          bordercolor : "",
+          cornerradius : 10
+        }
+
+        const styleModel = Object.assign( defaultValue, data );
+
         drawBg( styleModel );
 
         _styleType = styleModel.style;
 
         var grpid = data[ "grpid" ];
-        var urlCounterInfo = "CounterInfo.xml";
-        loadXml( urlCounterInfo );
+        loadXml( URL_COUNTER_STAT_INFO );
       });
     }
 
@@ -29,12 +45,20 @@
         url: strUrl,
         dataType: "xml",
         success: function(doc) {
-          var visitor = doc.getElementsByTagName( "visitor" )[0].textContent;
-          var article = doc.getElementsByTagName( "article" )[0].textContent;
-          var joiner = doc.getElementsByTagName( "joiner" )[0].textContent;
+          const defaultValue = {
+            visitor : 0,
+            article : 0,
+            joiner : 0
+          }
           
-          var statsModel = new StatsModel( visitor, article, joiner );
-          console.log( statsModel );
+          var statsModel = {
+            visitor : doc.getElementsByTagName( "visitor" )[0].textContent,
+            article : doc.getElementsByTagName( "article" )[0].textContent,
+            joiner : doc.getElementsByTagName( "joiner" )[0].textContent,
+          }
+
+          statsModel = Object.assign( defaultValue, statsModel );
+
           drawStats( statsModel );
         }
       });
@@ -43,11 +67,13 @@
     function drawBg( styleModel ) {
       var bg = document.getElementById( "counterBgContainer" );
 
-      bg.style.width = styleModel.width;
-      bg.style.height = styleModel.height;
+      const PIXEL = "px";
+
+      bg.style.width = styleModel.width + PIXEL;
+      bg.style.height = styleModel.height + PIXEL;
       bg.style.backgroundColor = styleModel.backgroundcolor;
       bg.style.opacity = styleModel.backgroundalpha;
-      bg.style.borderRadius = styleModel.cornerradius;
+      bg.style.borderRadius = styleModel.cornerradius + PIXEL;
 
       if( styleModel.bordertype ) {
         bg.style.borderStyle = "solid";
--- scripts/models/StatsModel.js
+++
@@ -1,5 +0,0 @@
-function StatsModel( nVisitor, nArticle, nJoiner ) {
-    this.visitor = nVisitor;
-    this.article = nArticle;
-    this.joiner = nJoiner;
-}
